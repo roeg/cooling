983d99e33f6a1b09eae568284638e470281d9ea3
robert <robert.egger@nyumc.org>
2020-04-20 13:12:26 -0400

    first version of hierarchical network cooling

--- cool_cpg.py
+++ cool_cpg.py
@@ -220,6 +220,116 @@ def cool_distributed_cpgs():
     plt.show()
 
 
+def cool_hierarchical_cpgs():
+    """
+    main function to cool single recurrent network with external top-down input.
+    cool either external input or cpg itself.
+    loads previously fitted output weights, computes correlation between neural trajectories
+    and output before/during cooling as similarity measures.
+    :return: nothing
+    """
+
+    # load output weights
+    out_dir = '/Users/robert/project_src/cooling/single_cpg_manipulation/weights'
+    weight_suffix1 = 'outunit1_weights_force_w200_2w_input_w200.npy'
+    weight_suffix2 = 'outunit2_weights_force_w200_2w_input_w200.npy'
+    weight_suffix3 = 'Wrec_weights_force_w200_2w_input_w200.npy'
+    Wout1 = np.load(os.path.join(out_dir, weight_suffix1))
+    Wout2 = np.load(os.path.join(out_dir, weight_suffix2))
+    Wrec = np.load(os.path.join(out_dir, weight_suffix3))
+
+    # create network with same parameters
+    t_max = 2000.0
+    dt = 1.0
+    nw = rn.Network(N=800, g=1.5, pc=1.0)
+    nw.Wrec = Wrec
+
+    w_ext = 2 * np.pi / 200.0 # control slow timescale like this maybe?
+    amp_ext = 0.05
+    def ext_inp(t):
+        # w = 2 * np.pi / 200.0 # control slow timescale like this maybe?
+        return amp_ext * np.ones(nw.N) * np.sin(w_ext * t)
+
+    # run dynamics at reference temperature and compute neural/behavioral trajectory
+    ref_t, ref_rates = nw.simulate_network(T=t_max, dt=dt, external_input=ext_inp)
+    ref_mean_ = np.mean(ref_rates, axis=1)
+    ref_mean = ref_mean_.transpose()
+    # ref trajectory: first three PCs (sufficient?)
+    pcs, ref_trajectory = rn.compute_neural_trajectory(ref_rates)
+    neuron_out1 = np.dot(Wout1, ref_rates)
+    neuron_out2 = np.dot(Wout2, ref_rates)
+    ref_behavior = neuron_out1 * neuron_out2
+
+    fig1 = plt.figure(1)
+    ax1 = fig1.add_subplot(2, 1, 1)
+    ax1.plot(ref_trajectory[0, :], ref_trajectory[1, :], 'k', linewidth=0.5, label='ref')
+    ax2 = fig1.add_subplot(2, 1, 2)
+    ax2.plot(ref_t, ref_behavior, 'k', linewidth=0.5, label='ref')
+
+
+    # run dynamics at different temperatures using some Q10 for tau
+    # and compute neural/behavioral trajectories
+    # dT_steps = [-0.2, -0.5, -1.0, -1.5, -2.0, -2.5, -3.0, -3.5, -4.0, -4.5, -5.0]
+    dT_steps = [-1.0, -3.0, -5.0]
+    # dT_steps = [-5.0, -10.0, -20.0]
+    # dT_steps = [-0.5, -1.0, -2.0]
+    fig2 = plt.figure(2)
+    ax3 = fig2.add_subplot(len(dT_steps) + 1, 1, 1)
+    for i in range(10):
+        ax3.plot(ref_t, ref_rates[i, :], linewidth=0.5)
+    cooled_trajectories = []
+    cooled_behaviors = []
+    for i, dT in enumerate(dT_steps):
+        cooled_q = _q(dT)
+        cooled_nw = rn.Network(N=800, g=1.5, pc=1.0)
+        cooled_nw.Wrec = Wrec
+
+        # only cool upstream network
+        def ext_inp_cool(t):
+            w_cool = w_ext * cooled_q
+            return amp_ext * np.ones(nw.N) * np.sin(w_cool * t)
+
+        cooled_t, cooled_rates = cooled_nw.simulate_network(T=t_max/cooled_q, dt=dt, external_input=ext_inp_cool)
+
+        behavior = np.dot(Wout1, cooled_rates) * np.dot(Wout2, cooled_rates)
+        cooled_behaviors.append(behavior)
+        projected_rates = rn.project_neural_trajectory(cooled_rates, ref_mean, pcs)
+        cooled_trajectories.append(projected_rates)
+
+        label_str = 'dT = %.1f' % dT
+        ax1.plot(projected_rates[0, :], projected_rates[1, :], linewidth=0.5, label=label_str)
+        ax2.plot(cooled_t, behavior, linewidth=0.5, label=label_str)
+        ax3 = fig2.add_subplot(len(dT_steps) + 1, 1, 2 + i)
+        for j in range(10):
+            ax3.plot(cooled_t, cooled_rates[j, :], linewidth=0.5)
+
+    ax1.legend()
+    ax1.set_xlabel('PC 1 (a.u.)')
+    ax1.set_ylabel('PC 2 (a.u.)')
+    ax2.legend()
+    ax2.set_xlabel('Output (a.u.)')
+
+    # measure similarity of neural/behavioral trajectories as a function of temperature
+    trajectory_similarities = []
+    behavior_similarities = []
+    for i in range(len(dT_steps)):
+        similarity1 = rn.measure_trajectory_similarity(ref_trajectory, cooled_trajectories[i])
+        similarity2 = rn.measure_trajectory_similarity(ref_behavior, cooled_behaviors[i])
+        trajectory_similarities.append(similarity1)
+        behavior_similarities.append(similarity2)
+    fig4 = plt.figure(4)
+    ax4 = fig4.add_subplot(1, 1, 1)
+    ax4.plot(dT_steps, trajectory_similarities, 'ko-', label='neural activity')
+    ax4.plot(dT_steps, behavior_similarities, 'ro-', label='behavior')
+    ax4.set_xlim(ax4.get_xlim()[::-1])
+    ax4.set_xlabel('Temperature change')
+    ax4.set_ylabel('Corr. coeff.')
+    ax4.legend()
+
+    plt.show()
+
+
 if __name__ == '__main__':
-    cool_single_cpg()
+    # cool_single_cpg()
     # cool_distributed_cpgs()
+    cool_hierarchical_cpgs()
